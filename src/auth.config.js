8d348b9e120201af82cced03e78249ca430b9cd5
seoyonara <syp614@ewhain.net>
2025-07-11 00:20:37 +0900

    feat: 구글 로그인 세팅

--- src/auth.config.js
+++ src/auth.config.js
@@ -1,1 +1,58 @@
 import { PrismaClient } from './db.config.js';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+const passport = require('passport');
+
+const prisma = new PrismaClient();
+
+export const googleStrategy = new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+
+    callbackURL: 'http://localhost:3000/auth/google/callback', //인증 후 콜백 URL
+
+    scope: ['profile', 'email'], //요청할 권한
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    return googleVerify(profile, accessToken, refreshToken) //프로필을 이용해 유저를 검증하고, 반환된 유저 정보를 콜백함수로 전달
+      .then((user) => done(null, user)) //성공 시 유저 정보 전달
+      .catch((err) => done(err)); //실패 시 에러 전달
+  }
+);
+
+const googleVerify = async (profile, accessToken, refreshToken) => {
+  try {
+    const email = profile.emails?.[0]?.value; //구글 프로필에서 이메일 추출
+    if (email == false) {
+      throw new NotFoundError(`profile.email was not found: ${profile}`); // 이메일이 없으면 에러 처리
+    }
+
+    const user = await prisma.users.findUnique({ where: { email } });
+    if (user !== null) {
+      // 이미 유저가 존재하면 해당 유저 정보 반환
+      return { id: user.user_id, email: user.email, accessToken, refreshToken };
+    }
+
+    const createdUser = await prisma.user.create({
+      data: {
+        user_id: profile.id, //네이버에서 주는 id값을 user_id로 저장
+        email,
+        name: profile.name || ' ', // 프로필 이름이 없으면 빈 문자열로 처리
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    });
+    return {
+      id: createdUser.user_id,
+      email: createdUser.email,
+      accessToken,
+      refreshToken,
+    }; // 새로 생성된 유저의 ID와 이메일 반환
+  } catch (err) {
+    throw new ValidationError('인증 과정 중 에러 발생');
+  }
+};
+
+passport.use(googleStrategy);
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
---
+++ src/middlewares/authRouter.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import redis from '../config/redis.js';
+import { authenticate } from 'passport';
+import { generateAccessToken, generateRefreshToken } from '../utils/jwt.js';
+
+const router = express.Router();
+
+router.get('/auth', authenticate('google', { scope: ['profile', 'email'] })); // 프로파일과 이메일 정보를 받는다.
+
+router.get(
+  '/auth/google/callback',
+  authenticate('google', { failureRedirect: '/' }),
+  async (req, res) => {
+    const user = req.user;
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user);
+
+    await redis.set(`refresh:${user.id}`, refreshToken, 'EX', 7 * 24 * 60 * 60);
+
+    res.json({ accessToken, refreshToken });
+  }
+);
+
+export default router;
