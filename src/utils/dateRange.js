0270b29b6b20c05f66b6df64c3a38dfb4a82b887
snahpaek <ccyberneticss@gmail.com>
2025-08-11 05:19:05 +0900

    ✨ 타임존 변환 유틸 함수 구현

--- src/utils/dateRange.js
+++ src/utils/dateRange.js
@@ -5,7 +5,13 @@ import timezone from 'dayjs/plugin/timezone.js';
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-/// KST 기준으로 하루의 시작과 끝 범위를 반환하는 함수
+// UTC >> KST 변환
+export const utcToKstDate = (input) => {
+  const d = input instanceof Date ? input : new Date(input);
+  return new Date(d.getTime() + 9 * 60 * 60 * 1000);
+};
+
+// KST 기준으로 하루의 시작과 끝 범위 반환
 export const getKSTDateRange = (dateStr) => {
   const start = dayjs.tz(`${dateStr} 00:00:00`, 'Asia/Seoul').toDate();
   const end = dayjs.tz(`${dateStr} 23:59:59.999`, 'Asia/Seoul').toDate();
