7dcf20b133c0c6f27430f238f08aebcc7dfe3826
김민지 <meeji12345@naver.com>
2025-08-13 13:57:33 +0900

    expo push token 저장 api 구현

--- src/controllers/alarm.controller.js
+++ src/controllers/alarm.controller.js
@@ -1,11 +1,11 @@
-import { NotFoundError } from '../errors/error.js';
-import { getWakeUpAlarmDTO } from '../dtos/wakeupalarm.dto.js';
+import { ForbiddenError, NotFoundError } from '../errors/error.js';
+import { getWakeUpAlarmDTO, pushWakeUpAlarmDTO } from '../dtos/wakeupalarm.dto.js';
 import { getMyalarmDTO } from '../dtos/myalarm.dto.js';
 import { getAutoAlarmDTO } from '../dtos/autoalarm.dto.js';
 import { getWakeUpAlarmByUserId } from '../services/wakeupalarm.service.js';
 import { getMyAlarmByUserId } from '../services/myalarm.service.js';
 import { getAutoAlarmByUserId } from '../services/autoalarm.service.js';
-import { StatusCodes } from 'http-status-codes';
+import { savePushTokenService } from '../services/alarm.service.js';
 import { prisma } from '../db.config.js';
 
 // 알람 조회 API
@@ -49,5 +49,21 @@ export const getAllAlarm = async (req, res, next) => {
         return res.success(responseData);
     }catch (error) {
         next(error);
+    }
+};
+
+// 푸시 토큰 저장
+export const pushTokenAlarm = async (req, res, next) => {
+    try {
+      const { token } = req.body;
+      const userId = req.user?.user_id;
+  
+      if (!token) {
+        throw new ForbiddenError('푸시 알람 토큰이 없습니다.');
+      }
+      await savePushTokenService(userId, token);
+      res.success(userId, console.log('푸시 알람 토큰 저장 성공'));
+    } catch (error) {
+      next(error);
     }
 };
