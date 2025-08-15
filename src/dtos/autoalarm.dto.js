f8d34a79b59bd15d409f5744625504e4dbebb4ff
김민지 <meeji12345@naver.com>
2025-08-15 15:28:42 +0900

    알람 조회 api 수정

--- src/dtos/autoalarm.dto.js
+++ src/dtos/autoalarm.dto.js
@@ -53,6 +53,7 @@ export const createAutoAlarmDTO = (user_id, body) => {
   
     return {
       user_id: AutoAlarm.user_id,
+      auto_alarm_id: AutoAlarm.auto_alarm_id,
       wakeup_time: date,
       is_active: AutoAlarm.is_active,
     };
@@ -62,6 +63,7 @@ export const createAutoAlarmDTO = (user_id, body) => {
   export const getMPAutoAlarmDTO = (AutoAlarm, ATalarmId) => {
     return {
       user_id: AutoAlarm.user_id,
+      auto_alarm_id: ATalarmId,
       vibration_type: AutoAlarm.vibration_type,
       sound_id: AutoAlarm.sound_id,
       repeat_interval: AutoAlarm.repeat_interval,
