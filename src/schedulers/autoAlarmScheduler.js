ecf16b45f756495c20dd3ac9a4eaa2e11c14122b
tqios <80325001+tqios@users.noreply.github.com>
2025-08-19 20:18:28 +0900

    Update autoAlarmScheduler.js

--- src/schedulers/autoAlarmScheduler.js
+++ src/schedulers/autoAlarmScheduler.js
@@ -6,7 +6,7 @@ import { getAllUserIds } from '../repositories/user.repository.js';
 
 export function startAutoAlarmScheduler() {
   //분, 시, 일, 월, 요일
-  cron.schedule('15 20 * * *', async () => {
+  cron.schedule('30 20 * * *', async () => {
     try {
       console.log('auto alarm scheduler start1');
       const userIds = await getAllUserIds();
@@ -16,9 +16,11 @@ export function startAutoAlarmScheduler() {
     } catch (err) {
       console.error('오류 발생:', err);
     }
-  });
+  , {
+  timezone: "Asia/Seoul"
+});
 
-  cron.schedule('12 11 * * *', async () => {
+  cron.schedule('31 11 * * *', async () => {
     try {
       console.log('auto alarm scheduler start2');
       const userIds = await getAllUserIds();
