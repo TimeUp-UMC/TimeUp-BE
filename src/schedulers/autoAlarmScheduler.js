83277eac69515e9941494deb465460602926ec46
tqios <80325001+tqios@users.noreply.github.com>
2025-08-19 20:22:22 +0900

    Update autoAlarmScheduler.js

--- src/schedulers/autoAlarmScheduler.js
+++ src/schedulers/autoAlarmScheduler.js
@@ -1,24 +1,28 @@
 // schedulers/autoAlarmScheduler.js
 
-import cron from 'node-cron'; //npm install node-cron
+import cron from 'node-cron'; // npm install node-cron
 import { addAutoAlarmService } from '../services/autoalarm.service.js';
 import { getAllUserIds } from '../repositories/user.repository.js';
 
 export function startAutoAlarmScheduler() {
-  //분, 시, 일, 월, 요일
-  cron.schedule('30 20 * * *', async () => {
-    try {
-      console.log('auto alarm scheduler start1');
-      const userIds = await getAllUserIds();
-      for (const userId of userIds) {
-        await addAutoAlarmService({ userId });
+  // 20:30 (KST)
+  cron.schedule(
+    '30 20 * * *',
+    async () => {
+      try {
+        console.log('auto alarm scheduler start1');
+        const userIds = await getAllUserIds();
+        for (const userId of userIds) {
+          await addAutoAlarmService({ userId });
+        }
+      } catch (err) {
+        console.error('오류 발생:', err);
       }
-    } catch (err) {
-      console.error('오류 발생:', err);
+    },
+    {
+      timezone: 'Asia/Seoul',
     }
-  , {
-  timezone: "Asia/Seoul"
-});
+  );
 
   cron.schedule('31 11 * * *', async () => {
     try {
